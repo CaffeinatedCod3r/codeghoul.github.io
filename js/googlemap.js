a3076db7a92a5558033c02f40b5065b118fe71c3
codeghoul1 <jayesh.patel.2197@gmail.com>
2018-10-24 21:33:58 +0530

    add google map support

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,8 +1,23 @@
-function initMap()
-{
-  // The location of Uluru
-  var uluru = {lat: 21.190449, lng: 81.284920};
+function getUluru() {
+	let uluru = {lat: 21.190449, lng: 81.284920};
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(setPosition)
+	}
+	return uluru;
+}
+
+function setPosition(position) {
+    uluru = {
+				lat: +position.coords.latitude,
+				lng: +position.coords.longitude
+		}
+}
+
+function initMap() {
+	// The location of Uluru
+	let uluru = getUluru(); 
   // The map, centered at Uluru
+  console.log(uluru);
   var map = new google.maps.Map(
   	document.getElementById('map'), {zoom: 10, center: uluru});
   // The marker, positioned at Uluru
